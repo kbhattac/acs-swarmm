ea1ff7c747f44e8125e5fa044d6bf3d17bd1e66c
Kaushik Bhattacharya <kbhatt@microsoft.com>
2017-02-28 21:52:32 +0100

    adding prep script

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 Swarm Mode orchestrator with VMSS, Managed Disks, Custom VNETs
 ## Requires pre-provisioned resource group with VNET and subnets
 - See /scripts dir for provisioning scripts
+- Use /scripts/prep.sh for pre-provisioining resources before clicking on **Deploy to Azure**
 - Generated with [acs-engine](https://github.com/Azure/acs-engine)
 - Installs Docker CS
+
+[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fkbhattmsft%2Facs-swarmm%2Fmaster%2Fazuredeploy.json)
---
+++ scripts/prep.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+createRG()
+{
+    az group create \
+        --name acs-swarmm-vnet \
+        --location westeurope 
+}
+
+createVnet() 
+{
+    az network vnet create \
+        --resource-group acs-swarmm-vnet \
+        --location westeurope \
+        --name swarmm-vnet \
+        --address-prefix 172.17.0.0/24
+}
+
+createMasterSubnet()
+{
+    az network vnet subnet create \
+        --name master \
+        --resource-group acs-swarmm-vnet \
+        --vnet-name swarmm-vnet \
+        --address-prefix 172.17.0.0/25
+        --query "[?provisioningState=='Succeeded'].{ id: id }"
+}
+
+createAgentSubnet()
+{
+    az network vnet subnet create \
+        --name agent \
+        --resource-group acs-swarmm-vnet \
+        --vnet-name swarmm-vnet \
+        --address-prefix 172.17.0.128/25
+        --query "[?provisioningState=='Succeeded'].{ id: id }"
+}
+
+
+time createRG
+time createVnet
+time createMasterSubnet
+time createAgentSubnet
